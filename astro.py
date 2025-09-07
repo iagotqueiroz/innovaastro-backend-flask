b7cccfe42e4862455af469115029272de5bf9ad1
Felicio da Silva Araujo <felicio.sa@gmail.com>
2025-09-07 00:14:56 -0300

    grava uma requisição de movimento dos motores no banco de dados

--- astro.py
+++ astro.py
@@ -18,13 +18,17 @@ def buscar_astro(latitude,longitude,nome_astro):
     
     alt, az, distancia = astrometrico.apparent().altaz()
 
-    rastrear(az,alt)
+    # rastrear(az,alt)
+
+    url = f"https://boout-n8n.sifxgd.easypanel.host/webhook/fe5a4408-3951-4124-90b0-ddfd26cc11ed?nome_astro={nome_astro}&latitude={latitude}&longitude={longitude}&az={az.degrees:.2f}&alt={alt.degrees:.2f}"
+    requests.get(url, timeout=2)
     
     return {"name":nome_astro,"latitude":latitude,"longitude":longitude,"alt":alt.degrees,"az":az.degrees,"distancia":distancia.km}
 
 def rastrear(az,alt):
     try:
-        url = f"http://10.13.37.2/mover?az={az.degrees:.2f}&alt={alt.degrees:.2f}"
+        url = f"http://10.13.37.2:8180/mover?az={az.degrees:.2f}&alt={alt.degrees:.2f}"
+        url = f"https://boout-n8n.sifxgd.easypanel.host/webhook-test/fe5a4408-3951-4124-90b0-ddfd26cc11ed?az={az.degrees:.2f}&alt={alt.degrees:.2f}"
         requests.get(url, timeout=2)
     except Exception as e:
         print(f"[ESP32] Falha: {e}")
